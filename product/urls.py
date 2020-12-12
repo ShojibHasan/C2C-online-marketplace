f169d04e84cce52ad670d86deb492704603cfabc
shojibhasan <shojibhasan15@gmail.com>
2020-12-12 19:25:07 +0600

    last commit

--- product/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.productlist,name='product_list'),
-    path('<id:int>',views.productdetail,name='product_detials'),
-]
